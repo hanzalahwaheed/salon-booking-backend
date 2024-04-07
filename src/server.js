8ac9f16d1f306e902dd0e9391cbd5c3c650886cb
hanzalahwaheed <hanzalah.w@gmail.com>
2024-04-07 11:34:56 +0400

    Establish file structure

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
