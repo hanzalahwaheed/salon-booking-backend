0ffc40e5397e8cc6254b6ff8aa0b0fb1b0bb0051
hanzalahwaheed <hanzalah.w@gmail.com>
2024-04-08 15:25:59 +0400

    get available slots api done

--- src/controllers/availabilityController.js
+++ src/controllers/availabilityController.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 
 export const setAvailability = async (req, res) => {
   const email = req.email;
-  
+
   const user = await User.findOne({ email });
   if (!user) return res.json("User not Found");
   if (!user.isAdmin) return res.json("User unauthorized");
@@ -18,15 +18,44 @@ export const setAvailability = async (req, res) => {
         date,
         day,
         maxCapacity,
-        slots: [{ start: slotStart, end: slotEnd }],
+        slots: [{ start: slotStart, end: slotEnd, maxCapacity }],
       });
     } else {
-      availability.slots.push({ start: slotStart, end: slotEnd });
+      availability.slots.push({ start: slotStart, end: slotEnd, maxCapacity });
       await availability.save();
     }
 
-    res.status(200).json(availability);
+    res
+      .status(200)
+      .json({
+        status: true,
+        message: "availability set",
+        availability: availability,
+      });
   } catch (error) {
     res.status(500).json({ error: "Failed to set availability" });
+  }
+};
+
+export const getAvailability = async (req, res) => {
+  const date = req.params.date;
+  try {
+    const availability = await Availability.findOne({ date });
+    console.log(availability);
+
+    if (!availability) {
+      // No availability found for the given date
+      return res
+        .status(404)
+        .json({ message: "No availability found for the given date" });
+    }
+
+    const freeSlots = availability.slots.filter(
+      (slot) => slot.currentCapacity < slot.maxCapacity
+    );
+    res.status(200).json(freeSlots);
+  } catch (error) {
+    console.error("Error retrieving availability:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- src/routes/availabilityRoutes.js
+++ src/routes/availabilityRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { setAvailability } from "../controllers/availabilityController.js";
+import { setAvailability,getAvailability } from "../controllers/availabilityController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/set", authMiddleware, setAvailability);
+router.get("/slots/:date", authMiddleware, getAvailability);
 
 export default router;
